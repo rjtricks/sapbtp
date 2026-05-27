60742062adcd44a8b0f5ad59f443f71c889123ae
RJ Tricks <rjtricksindia@gmail.com>
2026-05-27 12:26:07 +0000

    Hello World

--- productshop/srv/service.js
+++ productshop/srv/service.js
@@ -2,5 +2,6 @@ const cds = require('@sap/cds');
 module.exports = cds.service.impl(async function(srv) {
     srv.on('printHelloWorld', req => {
         console.log(req.data)
+        return `${req.data.input} World!`
     })
 })
